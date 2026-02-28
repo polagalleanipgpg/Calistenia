d092de5239373b7fd20a1c00e6a22697981923f6
Pablo Galleani <polagalleanipgpg@gmail.com>
2026-02-28 03:24:23 -0300

    chore: redirect root to /login to avoid unauthenticated 404

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  redirect('/dashboard')
+  // Default to the login page so unauthenticated visitors don't get a 404
+  redirect('/login')
 }
